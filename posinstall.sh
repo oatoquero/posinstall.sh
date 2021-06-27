51d43a407eca96cccf926dc66720e42636885d7d
oatoquero <86561692+oatoquero@users.noreply.github.com>
2021-06-27 11:23:42 -0300

    Add files via upload

---
+++ posinstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+PACOTES_APT="(net-tools build-essential)"
+
+apt update
+
+cd $(mktemp -d)
+${PACOTES_APT[@]}
+
+apt dist-upgrade -y
+apt autoclean 
+
+echo "Chegamos ao final"
+
+exit 0
